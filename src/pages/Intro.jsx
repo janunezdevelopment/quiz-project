6451cdb97cfa7f3783edca01a3b91b4c3079cd9a
janunezdevelopment <157073673+janunezdevelopment@users.noreply.github.com>
2026-04-09 16:13:09 -0400

    Refactor Intro component to include difficulty selection label and improve styling and accessibility.

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -3,7 +3,12 @@ function Intro({ startGame }) {
     <div className="intro">
       <h1 className="text-color-dark-blue karla-font">Quizzical</h1>
       <span className="text-color-dark-blue">A quiz about computer tech</span>
-      <select className="text-color-dark-blue">
+      <label htmlFor="difficulty">Select Difficulty:</label>
+      <select
+        name="difficulty"
+        id="difficulty"
+        className="text-color-dark-blue"
+      >
         <option value="easy">Easy</option>
         <option value="medium">Medium</option>
         <option value="hard">Hard</option>
